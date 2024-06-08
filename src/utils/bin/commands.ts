c7941734f23a4979f4bc97b6ca8e44cc1dbfc519
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-06-08 20:13:25 +0000

    feat: Add commands for terminal-themed website
    This commit adds a new file `commands.ts` that contains various commands for a terminal-themed website. These commands include functionalities like displaying help, opening the Github repository, providing information about the developer, opening the resume, donating, contacting via email, opening Github and LinkedIn profiles, searching on Google, DuckDuckGo, Bing, and Reddit, and executing typical Linux commands. The commit also includes a banner with ASCII art and a profile image.

---
+++ src/utils/bin/commands.ts
@@ -0,0 +1,186 @@
+// List of commands that do not require API calls
+
+import * as bin from './index';
+import config from '../../../config.json';
+
+import ProfileImg from '../../assets/profile.jpeg';
+
+// Help
+export const help = async (args: string[]): Promise<string> => {
+  const commands = Object.keys(bin).sort().join(', ');
+  var c = '';
+  for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
+    if (i % 7 === 0) {
+      c += Object.keys(bin).sort()[i - 1] + '\n';
+    } else {
+      c += Object.keys(bin).sort()[i - 1] + ' ';
+    }
+  }
+  return `Welcome!
+Here are all the available commands:
+\n${c}\n
+[tab]: trigger completion.
+[ctrl+l]/clear: clear terminal.\n
+Type 'sumfetch' to display summary.
+`;
+};
+
+// Redirection
+export const repo = async (args: string[]): Promise<string> => {
+  window.open(`${config.repo}`);
+  return 'Opening Github repository...';
+};
+
+// About
+export const about = async (args: string[]): Promise<string> => {
+  return `Hi, I am ${config.name}. 
+Welcome to my terminal themed website!
+More about me:
+'sumfetch' - short summary.
+'resume' - my latest resume.
+'readme' - my github readme.`;
+};
+
+export const resume = async (args: string[]): Promise<string> => {
+  window.open(`${config.resume_url}`);
+  return 'Opening resume...';
+};
+
+// Donate
+export const donate = async (args: string[]): Promise<string> => {
+  return `thank you for your interest. 
+here are the ways you can support my work:
+- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
+- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
+`;
+};
+
+// Contact
+export const email = async (args: string[]): Promise<string> => {
+  window.open(`mailto:${config.email}`);
+  return `Opening mailto:${config.email}...`;
+};
+
+export const github = async (args: string[]): Promise<string> => {
+  window.open(`https://github.com/${config.social.github}/`);
+
+  return 'Opening github...';
+};
+
+export const linkedin = async (args: string[]): Promise<string> => {
+  window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
+
+  return 'Opening linkedin...';
+};
+
+// Search
+export const google = async (args: string[]): Promise<string> => {
+  window.open(`https://google.com/search?q=${args.join(' ')}`);
+  return `Searching google for ${args.join(' ')}...`;
+};
+
+export const duckduckgo = async (args: string[]): Promise<string> => {
+  window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
+  return `Searching duckduckgo for ${args.join(' ')}...`;
+};
+
+export const bing = async (args: string[]): Promise<string> => {
+  window.open(`https://bing.com/search?q=${args.join(' ')}`);
+  return `Wow, really? You are using bing for ${args.join(' ')}?`;
+};
+
+export const reddit = async (args: string[]): Promise<string> => {
+  window.open(`https://www.reddit.com/search/?q=${args.join(' ')}`);
+  return `Searching reddit for ${args.join(' ')}...`;
+};
+
+// Typical linux commands
+export const echo = async (args: string[]): Promise<string> => {
+  return args.join(' ');
+};
+
+export const whoami = async (args: string[]): Promise<string> => {
+  return `${config.ps1_username}`;
+};
+
+export const ls = async (args: string[]): Promise<string> => {
+  return `A
+list
+of
+fake
+directories
+that
+are
+used
+for
+demonstration
+purposes
+only`;
+};
+
+export const cd = async (args: string[]): Promise<string> => {
+  return `Unfortunately, I cannot afford more directories.
+If you want to help, you can type 'donate'.`;
+};
+
+export const date = async (args: string[]): Promise<string> => {
+  return new Date().toString();
+};
+
+export const vi = async (args: string[]): Promise<string> => {
+  return `Woah!, you still use 'vi'?
+Just try 'vim'.`;
+};
+
+export const vim = async (args: string[]): Promise<string> => {
+  return `'vim' is so outdated. 
+ How about 'nvim'?`;
+};
+
+export const nvim = async (args: string[]): Promise<string> => {
+  return `'nvim'? too fancy!
+ Why not 'emacs'?`;
+};
+
+export const emacs = async (args?: string[]): Promise<string> => {
+  return `You know what? 
+Just use vscode.`;
+};
+
+export const sudo = async (args?: string[]): Promise<string> => {
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
+  return `Permission denied: with little power comes... no responsibility? `;
+};
+
+// Banner
+export const banner = (args?: string[]): string => {
+  return `
+  
+  
+<div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+  <img class="profile-image" style="border-radius: 5%;" src="${ProfileImg.src}"  width= "500px"> 
+
+                   
+  <pre class="ascii-art">
+
+   █████████                                   █████                                   █████     
+  ███░░░░░███                                 ░░███                                   ░░███      
+ ░███    ░███ █████ ███ █████ ██████           ░███  ██████   █████   ██████  ████████ ░███████  
+ ░███████████░░███ ░███░░███ ███░░███          ░███ ███░░███ ███░░   ███░░███░░███░░███░███░░███ 
+ ░███░░░░░███ ░███ ░███ ░███░███████           ░███░███ ░███░░█████ ░███████  ░███ ░███░███ ░███ 
+ ░███    ░███ ░░███████████ ░███░░░      ███   ░███░███ ░███ ░░░░███░███░░░   ░███ ░███░███ ░███ 
+ █████   █████ ░░████░████  ░░██████    ░░████████ ░░██████  ██████ ░░██████  ░███████ ████ █████
+░░░░░   ░░░░░   ░░░░ ░░░░    ░░░░░░      ░░░░░░░░   ░░░░░░  ░░░░░░   ░░░░░░   ░███░░░ ░░░░ ░░░░░ 
+                                                                              ░███               
+                                                                              █████              
+                                                                             ░░░░░               
+                                                                                
+  </pre>
+  
+  </div>
+
+Type 'help' to see the list of available commands.
+Type 'sumfetch' to display summary.
+Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
+`;
+};
